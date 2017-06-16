7c4c55806f5493f6419b5498bcf52c852450a722
wangyang <1140581193@qq.com>
2017-06-16 17:59:22 +0800

    添加cinemas文件

---
+++ src/components/manage/cinemas/Search.vue
@@ -0,0 +1,21 @@
+<template>
+<div>
+   <div style="margin-top: 10px;width:400px;">
+    <el-input placeholder="请输入内容" v-model="value">
+    <el-select v-model="select" slot="prepend" placeholder="请选择" style="width:100px">
+      <el-option label="姓名" value="1"></el-option>
+      <el-option label="性别" value="2"></el-option>
+      <el-option label="年龄" value="3"></el-option>
+    </el-select>
+    <el-button slot="append" icon="search" @click="searchStudent"></el-button>
+  </el-input>
+</div>
+</div>
+    </template>
+    
+    <script>
+    export default{
+    
+    }
+    </script>
+    <style></style>
