b7fb0045b979f24faee30f1cfa21407d64eea6a1
datou <337414932@qq.com>
2017-06-16 18:03:37 +0800

    增加了电影管理表格数据

--- src/components/manage/moviesAll/MoviesAll.vue
+++ src/components/manage/moviesAll/MoviesAll.vue
@@ -1,53 +1,56 @@
 <template>
-<template>
-   <div>
-       <h2>电影管理</h2>
-       <AddElement :show='show'></AddElement>
-       
-       
-       
-       
-       
-   </div>
+<div>
+    <h2>电影管理</h2>
+    <AddElement>
+        
+        
+    </AddElement>
+    <MoviesTable :show='show'></MoviesTable>
     
-    
-
-
+</div>
 
 
 </template>
-<script>
-//import {ajax} from "@/common/ajax";
-import AddElement from "./AddElement";
-export default {
-//    methods:{
-//        show:function(page=10){
-//            ajax({
-//                type:'get',
-//                url:'/moives/find',
-//                data:{},
-//                success:(data)=>{
-//                    store.commit("CINEMA_DATA",data);
-//                }
-//            })
-//        }
-//    },
-    components:{
-        AddElement
-//        ,DeleteElement,SearchElement,CinemaTable,PageNation
-    }
-}
+   
     
-</script>
-<style scoped>
+    
 
-</style>
+
 
 <script>
 
-
-
-
+import {ajax} from "@/components/common/ajax";
+import AddElement from "./AddElement";
+import MoviesTable from "./MoviesTable";
+import store from "@/store";
+export default {
+    created:function(){
+        this.show();
+    },
+    methods:{
+         show:function(page=1,type,value){
+             let obj={};
+             if(type){
+                 obj[type]=value;
+             }
+             obj.page = page;
+             obj.rows = 5;
+        ajax({
+            type:'get',
+            url:'movies/find',
+            data:obj,
+            success:(data)=>{
+               store.commit("MOVIESALL_DATA",data)
+            }
+        })  
+        }
+    },
+    components:{
+        AddElement,MoviesTable
+    }
+   
+        
+}
 
 
 </script>
