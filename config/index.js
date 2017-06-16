66d5e1936f85725eadff0590569004b39e11b97f
fengwangyang <1140581193@qq.com>
2017-06-16 14:34:25 +0800

    配置热古属3000端口

--- config/index.js
+++ config/index.js
@@ -27,7 +27,15 @@ module.exports = {
     autoOpenBrowser: true,
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    proxyTable: {},
+    proxyTable: {
+       '*' : {
+                target: 'http://127.0.0.1:3000',
+                changeOrigin: true,
+                filter: function (pathname, req) {
+                    return pathname.match('^(/users)|(/movies)|(/news)|(/logout)|(/cinemas)|(/hotshowing)|(/preonshow)|(/hotbroadcast)|(/linkedMovies)');
+                }
+        }
+    },
     // CSS Sourcemaps off by default because relative paths are "buggy"
     // with this option, according to the CSS-Loader README
     // (https://github.com/webpack/css-loader#sourcemaps)
