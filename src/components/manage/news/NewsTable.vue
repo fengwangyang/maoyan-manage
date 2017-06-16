1f9a0b62e935678d6527d5e5e893a2fb1053d422
wangmin <519808965@qq.com>
2017-06-16 20:43:20 +0800

    退出

--- src/components/manage/news/NewsTable.vue
+++ src/components/manage/news/NewsTable.vue
@@ -1,33 +1,13 @@
 <template lang="html">
-   <el-table :datas="datas" borderstyle="width: 100%">
-    
-     <el-table-column label="日期" width="180">
-      <template scope="scope">
-        <span style="margin-left: 10px">{{datas.username}}</span>
-      </template>
-    </el-table-column>
-    
-     <el-table-column label="姓名" width="180">
-      
-       <template scope="scope">
-        <el-popover trigger="hover" placement="top">
-          <p>姓名:</p>
-          <p>住址:</p>
-          <div slot="reference" class="name-wrapper">
-            <el-tag></el-tag>
-          </div>
-        </el-popover>
-      </template>
-    </el-table-column>
-    
-    
-    <el-table-column label="操作">
-      <template scope="scope">
-        <el-button size="small" >编辑</el-button>
-        <el-button size="small" type="danger">删除</el-button>
-      </template>
-    </el-table-column>
-  </el-table>
+      <el-row>
+          <el-table>
+              <el-table-column label='姓名'></el-table-column>
+              <el-table-column label='性别'></el-table-column>
+              <el-table-column label='年龄'></el-table-column>
+              <el-table-column label='操作'></el-table-column>
+          </el-table>
+          
+      </el-row>
 </template>
 
 
