9bacd1cc9cabcf898ccf36b141c674cf4634ef1f
tom <457246263@qq.com>
2017-06-17 16:44:52 +0800

    users增加功能，组件化

--- src/components/manage/users/Users.vue
+++ src/components/manage/users/Users.vue
@@ -1,38 +1,71 @@
 <template>
-
-<!--      <UsersManage></UsersManage>-->
+<div>
+        
+    <h1>用户管理</h1>
+      <UsersManage></UsersManage>
     <el-table
         :data="tableData"
         border
         style="width: 100%">
         <el-table-column
-          prop="name"
-          label="姓名"
-          width="180">
+          type="selection"
+          width="55">
+          
         </el-table-column>
         <el-table-column
+          prop="name"
+          label="姓名">
+        </el-table-column>
+        <el-table-column
+          prop="pwd"
+          label="密码">
+        </el-table-column>
+         <el-table-column
           prop="phNum"
-          label="电话"
-          width="180">
+          label="电话">
+        </el-table-column>
+        <el-table-column
+          prop="time"
+          label="生日">
         </el-table-column>
         <el-table-column
           prop="email"
-          label="邮箱"
-          width="180">
+          label="邮箱">
+        </el-table-column>
+        <el-table-column label="操作">
+          <template scope="scope">
+            <el-button
+              size="small"
+              icon='edit'
+              type='info'
+              @click="handleEdit(scope.$index, scope.row)">修改</el-button>
+            <el-button
+              size="small"
+              type="warning"
+              icon='delete'
+              @click="del(scope.row._id)">删除</el-button>
+          </template>
         </el-table-column>
   </el-table>
- 
+<!--  <Page :show="show"></Page>-->
+ </div>
 </template>
 <script>
     import {ajax} from "@/components/common/ajax"
-//    import UsersManage from "./UsersManage"
+//    import store from "@/store"
+//    import {mapState} from "vuex";
+    import UsersManage from "./UsersManage"
+//    import Page from "./Page"
     export default {
         data(){
             return {
                 name:"",
+                pwd:"",
                 phNum:"",
+                time:"",
                 email:"",
-                tabelData:[]
+                _id:"",
+               tableData:"",
             }                
         },
         created(){
@@ -44,19 +77,29 @@
                     type:"get",
                     url:"/users/find",
                     data:{
-                        rows:5,
-                        page:1
+                        
                     },
                     success:(data)=>{
-//                        console.log(data);
-                        this.tableData=data.rows;
-                        console.log(this.tableData);
+                        console.log(data);
+                        this.tableData = data;
                     }
                 })
+            },
+            del(id){
+                ajax({
+                    type:"post",
+                    url:"/users/del",
+                    data:{
+                        _id:id
+                    },
+                    success:function(){
+                        this.show();
+                    }.bind(this)
+                });
             }
         },
         components:{
-//            UsersManage
+            UsersManage
         }
   }
 </script>
--- src/store/users/index.js
+++ src/store/users/index.js
@@ -1,9 +1,25 @@
+import {SHOW_DATA,SHOW_USERS,FIND_USERS,SET_UPDATEVISIBLE} from "./mutations"
+
 const users = {
-    stata:{
-        
+    state:{
+        data:{curpage:1,maxpage:"",rows:[],eachpage:5},
+        users:{name:"",phNum:"",email:"",pwd:"",time:""},
+        findUsers:{},
+        updateVisible:false
     },
     mutations:{
-        
+        [SHOW_DATA](state,data){
+            state.data = data;
+        },
+        [SHOW_USERS](state,users){
+            state.users = users;
+        },
+        [FIND_USERS](state,student){
+            state.findUsers = users;
+        },
+        [SET_UPDATEVISIBLE](state,data){
+            state.updataVisible = data;
+        }
     }
 }
 export {users as default}
